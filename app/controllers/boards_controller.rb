dd62390a90d43eddeeb1733392353ebc67d45359
Yuri Mello <yuri.mcr@gmail.com>
2024-08-28 00:15:19 -0300

    add upload game #10

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,3 +1,30 @@
 class BoardsController < ApplicationController
-  def index;end
+  def index
+    @boards = Board.all
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def create
+    @board = Board.new(board_params)
+
+    if @board.save
+      UploadBoardJob.perform_async(@board.id)
+      redirect_to @board, notice: 'CSV file uploaded successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:csv)
+  end
 end
--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -1,7 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe "Boards", type: :request do
+  let!(:board) { create(:board) }
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "assigns all boards to @boards", :aggregate_failures do
+      get boards_path
+      expect(assigns(:boards)).to eq([board])
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET /new" do
+    it "assigns all boards to @boards", :aggregate_failures do
+      get new_board_path
+      expect(assigns(:board)).to be_a_new(Board)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET /show" do
+    it "assigns all boards to @boards", :aggregate_failures do
+      get board_path(board.id)
+      expect(assigns(:board)).to eq(board)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /create" do
+    context "with valid attributes" do
+      let(:csv) { fixture_file_upload('valid.csv', 'text/csv') }
+
+      it "creates a new board" do
+        expect {
+          post boards_path, params: { board: { csv: csv } }, as: :multipart
+        }.to change(Board, :count).by(1)
+      end
+
+      it "enqueues the UploadBoardJob" do
+        allow(UploadBoardJob).to receive(:perform_async)
+        post boards_path, params: { board: { csv: csv } }, as: :multipart
+        expect(UploadBoardJob).to have_received(:perform_async)
+      end
+
+      it "redirects to the new board" do
+        post boards_path, params: { board: { csv: csv } }, as: :multipart
+        expect(response).to redirect_to(Board.last)
+        expect(flash[:notice]).to eq('CSV file uploaded successfully.')
+      end
+
+      it "creates board state" do
+        expect {
+          post boards_path, params: { board: { csv: csv } }, as: :multipart
+        }.to change(BoardState, :count).by(1)
+      end
+    end
   end
 end
